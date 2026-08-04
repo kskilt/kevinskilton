df495029705cac157a75549e14f1426f51623528
Kevin Skilton <kskilton@within3.com>
2026-08-04 10:35:24 -0400

    Add tests for Post model/controller and a basic system test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index lists published posts but not drafts" do
+    get root_url
+    assert_response :success
+    assert_match posts(:published).title, response.body
+    assert_no_match posts(:draft).title, response.body
+  end
+
+  test "show renders a published post" do
+    get post_url(posts(:published))
+    assert_response :success
+    assert_match posts(:published).title, response.body
+  end
+
+  test "show 404s for a draft post" do
+    get post_url(posts(:draft))
+    assert_response :not_found
+  end
+
+  test "show 404s for an unknown slug" do
+    get post_url("does-not-exist")
+    assert_response :not_found
+  end
 end
